26311216e76ea5470e51776d4043282b7384bd8c
Chris Oldwood <gort@cix.co.uk>
2016-01-25 21:03:55 +0000

    Added script to compare the source files referenced by a CodeBlocks .cbp project file with the Visual C++ .vcproj version.

---
+++ CompareVCandCB.cmd
@@ -0,0 +1,49 @@
+@echo off
+rem ************************************************************
+rem
+rem Compare the Visual C++ and CodeBlocks project files and
+rem point anyout  mismatches.
+rem
+rem ************************************************************
+setlocal
+
+:handle_help_request
+if /i "%~1" == "-?"     call :usage & exit /b 0
+if /i "%~1" == "--help" call :usage & exit /b 0
+
+:count_args
+if /i "%~1" == "" call :usage & exit /b 0
+
+set folder=%~1
+ 
+if not exist "%folder%\*.cbp" echo ERROR: No .cbp project file found in "%folder%" & exit /b 1
+for %%f in ("%folder%\*.cbp") do set cbpFile=%%f
+
+if not exist "%folder%\*.vcproj" echo ERROR: No .vcproj project file found in "%folder%" & exit /b 1
+for %%f in ("%folder%\*.vcproj") do set vcprojFile=%%f
+
+set cbFileList=%TEMP%\CbFileList.txt
+grep -E "Unit filename" "%cbpFile%" | gawk -F"\42" "{ print $2 }" | gsort | uniq > "%cbFileList%" 
+if %errorlevel% neq 0 exit /b 1
+
+set vcFileList=%TEMP%\VcFileList.txt
+grep -E "RelativePath" "%vcprojFile%" | gawk -F"\42" "{ print $2 }" | sed "s/.\\//g" | gsort | uniq > "%vcFileList%"
+if %errorlevel% neq 0 exit /b 1
+
+echo %cbpFile% ^| %vcprojFile% 
+diff -i "%cbFileList%" "%vcFileList%" --side-by-side --suppress-common-lines --width 40
+
+:success
+exit /b 0
+
+rem ************************************************************
+rem Functions
+rem ************************************************************
+
+:usage
+echo.
+echo Usage: %~n0 ^<project folder^>
+echo.
+echo e.g.   %~n0 C:\Dev\Project
+echo.
+echo Note: vc71=vs2003, vc80=vs2005, vc90=vs2008, vc100=vs2010, vc110=vs2012 

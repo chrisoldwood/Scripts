33679ff84d8a8ff8d7f13834f07758f85d81673c
Chris Oldwood <gort@cix.co.uk>
2023-08-18 17:33:08 +0100

    Added check in BuildChoco that Chocolately is installed.

--- BuildChoco.cmd
+++ BuildChoco.cmd
@@ -14,6 +14,9 @@ if /i "%~1" == "--help" call :usage & exit /b 0
 if /i "%~1" == "" call :usage & exit /b 1
 
 :build
+where /q choco
+if !errorlevel! neq 0 goto :choco_missing
+
 pushd "%~dp1."
 
 if not exist "bin" (
@@ -21,7 +24,13 @@ if not exist "bin" (
 	if !errorlevel! neq 0 popd & exit /b 1
 )
 
-for /f %%f in (PkgList.txt) do (
+set "pkglist=PkgList.txt"
+if not exist "%pkglist%" (
+    echo ERROR: Package list "%pkglist%" is missing!
+    exit /b 1
+)
+
+for /f %%f in (%pkglist%) do (
 	if not exist "%%f" (
 		echo ERROR: Package artefact "%%f" not found
 		popd & exit /b 1
@@ -46,3 +55,8 @@ echo Usage: %~n0 ^<.nuspec file^>
 echo.
 echo e.g.   %~n0 project\Chocolatey\project.nuspec
 goto :eof
+
+:choco_missing
+echo ERROR: Chocolatey not installed or on the PATH.
+echo See https://chocolatey.org/install for instructions.
+exit /b !errorlevel!

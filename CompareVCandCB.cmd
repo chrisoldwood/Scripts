a428582e3d2c97d15fc870a0cf458e591de97896
Chris Oldwood <gort@cix.co.uk>
2023-10-12 21:15:22 +0100

    Simplified the way we extract the filenames in CompareVCandCB.

--- CompareVCandCB.cmd
+++ CompareVCandCB.cmd
@@ -5,7 +5,7 @@ rem Compare the Visual C++ and CodeBlocks project files and
 rem point anyout  mismatches.
 rem
 rem ************************************************************
-setlocal
+setlocal enabledelayedexpansion
 
 :handle_help_request
 if /i "%~1" == "-?"     call :usage & exit /b 0
@@ -14,8 +14,16 @@ if /i "%~1" == "--help" call :usage & exit /b 0
 :count_args
 if /i "%~1" == "" call :usage & exit /b 0
 
+:check_tools
+where /q grep
+if !errorlevel! neq 0 goto :tools_missing
+where /q gawk
+if !errorlevel! neq 0 goto :tools_missing
+where /q diff
+if !errorlevel! neq 0 goto :tools_missing
+
 set folder=%~1
- 
+
 if not exist "%folder%\*.cbp" echo ERROR: No .cbp project file found in "%folder%" & exit /b 1
 for %%f in ("%folder%\*.cbp") do set cbpFile=%%f
 
@@ -23,11 +31,11 @@ if not exist "%folder%\*.vcproj" echo ERROR: No .vcproj project file found in "%
 for %%f in ("%folder%\*.vcproj") do set vcprojFile=%%f
 
 set cbFileList=%TEMP%\CbFileList.txt
-grep -E "Unit filename" "%cbpFile%" | gawk -F"\42" "{ print $2 }" | sed "s/\\//\\\\/g" | sed "s/\\.*\\.\\\\//" | sort | uniq > "%cbFileList%" 
+grep -E "Unit filename" "%cbpFile%" | gawk -F"\42" "{ print $2 }" | grep -o -E "[a-zA-Z0-9_.]+$" | sort | uniq > "%cbFileList%" 
 if %errorlevel% neq 0 exit /b 1
 
 set vcFileList=%TEMP%\VcFileList.txt
-grep -E "RelativePath=" "%vcprojFile%" | gawk -F"\42" "{ print $2 }" | sed "s/\\.*\\.\\\\//" | sort | uniq > "%vcFileList%"
+grep -E "RelativePath=" "%vcprojFile%" | gawk -F"\42" "{ print $2 }" | grep -o -E "[a-zA-Z0-9_.]+$" | sort | uniq > "%vcFileList%"
 if %errorlevel% neq 0 exit /b 1
 
 echo %cbpFile% ^| %vcprojFile% 
@@ -46,3 +54,7 @@ echo Usage: %~n0 ^<project folder^>
 echo.
 echo e.g.   %~n0 C:\Dev\Project
 goto :eof
+
+:tools_missing
+echo ERROR: grep/gawk/diff not installed or on the PATH.
+exit /b !errorlevel!

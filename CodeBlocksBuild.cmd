59209c488c79f05ff67a21fd1cfb76835950289b
Chris Oldwood <gort@cix.co.uk>
2026-04-02 12:47:58 +0100

    Fixed the CodeBlocksBuild script for 64-bit installs of Code::Blocks

--- CodeBlocksBuild.cmd
+++ CodeBlocksBuild.cmd
@@ -1,14 +1,13 @@
 @echo off
-rem ************************************************************
+rem ***************************************************************************
 rem
 rem Script to build a CodeBlocks workspace
 rem
-rem NB: The CodeBlocks build is not command line based and so
-rem it returns immediately. This is also the reason why we leave
-rem the build window open because otherwise there is no build
-rem output.
+rem Note: The CodeBlocks build is not command line based and so it returns
+rem immediately. This is also the reason why we leave the build window open,
+rem because otherwise there is no build output.
 rem
-rem ************************************************************
+rem ***************************************************************************
 setlocal enabledelayedexpansion
 
 :handle_help_request
@@ -18,14 +17,14 @@ if /i "%~1" == "--help" call :usage & exit /b 0
 :check_args
 if /i "%~1" == "" call :usage & exit /b 1
 
-set codeBlocksProgram=%ProgramFiles%\CodeBlocks\codeblocks.exe
+set "codeBlocksProgram=%ProgramFiles%\CodeBlocks\codeblocks.exe"
 
-if not defined ProgramFiles(x86) goto :check_installed
-set codeBlocksProgram=%ProgramFiles(x86)%\CodeBlocks\codeblocks.exe
+if not defined "ProgramFiles(x86)" goto :check_installed
+set "codeBlocksProgram=%ProgramFiles(x86)%\CodeBlocks\codeblocks.exe"
 
 :check_installed
 if not exist "%codeBlocksProgram%" (
-	echo ERROR: CodeBlocks program not installed as "%codeBlocksProgram%"
+	echo ERROR: CodeBlocks program not installed under "(Program Files x86/x64)\CodeBlocks\codeblocks.exe"
 	exit /b 1
 )
 
@@ -38,9 +37,9 @@ if errorlevel 1 (
 :success
 exit /b 0
 
-rem ************************************************************
+rem ***************************************************************************
 rem Functions
-rem ************************************************************
+rem ***************************************************************************
 
 :usage
 echo.
